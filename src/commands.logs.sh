5504a42da7ee50724e6abe6a5044bcf5c4c251e1
KieranKaelin <kieran_kaelin@yahoo.co.uk>
2024-11-06 13:03:23 +0100

    fix: remove check around `logs` command

--- src/commands.logs.sh
+++ src/commands.logs.sh
@@ -3,13 +3,8 @@
 # SPDX-License-Identifier: MIT
 # SPDX-FileCopyrightText: 2022-2024 CYBERTEC PostgreSQL International GmbH <office@cybertec.at>
 
-source ./console.sh
 source ./runtime.sh
 
 command_logs() {
-  if [ ! -z "$(docker compose top)" ]; then
-    runtime_logs "$1"
-  else
-    error "Logs not available while not running ($(highlight "'./migrator up'") to start)"
-  fi
+  runtime_logs "$1"
 }
